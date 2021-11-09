c2513c8fe00223aa26101c1c02ea4ec5709a3d11
florencianionquepan <85314154+florencianionquepan@users.noreply.github.com>
2021-11-09 18:55:06 -0300

    Se añade la función preliminar cargarJuegos()
    Se diseña el arreglo con los 10 juegos inventados para  poder seguir avanzando con el proyecto.

--- mio.php
+++ mio.php
@@ -1,44 +1,5 @@
-
 <?php
 
-include ('tateti.php');
-
-seleccionarOpcion();
-
-function seleccionarOpcion (){
- 
-    do{
-        echo "------Menú de opciones------\n"
-            ."1) Jugar al tateti.\n"
-            ."2) Mostrar un juego.\n"
-            ."3) Mostrar el primer juego ganador.\n"
-            ."4) Mostrar porcentaje de Juegos ganados.\n"
-            ."5) Mostrar resumen de Jugador.\n"
-            ."6) Mostrar listado de juegos Ordenado por jugador O.\n"
-            ."7) Salir.\n";
-        
-        echo "Ingrese su eleccion: ";
-        $eleccion = trim(fgets(STDIN));
-        
-        switch($eleccion){
-            case 1:jugar();break;
-            case 2:;break;
-            case 3:;break;
-            case 4:;break;
-            case 5:;break;
-            case 6:;break;
-            case 7: echo "gracias por haber usado el programa";break;
-            default: echo "elección ingresada no valida, por favor ingrese otra\n";break;
-        }
-    }while($eleccion!=7);
-}
-
-
-/**Inicializa una estructura de datos con ejemplos de juegos y retorna 
- * la colección de juegos indexado de arreglos asociativos que almacena 
- * la información de los juegos que se jugaron. En principio todos los juegos se inventaron.
- */
-//arreglo $coleccionJuegos
 function cargarJuegos(){
     $coleccionJuegos=[];
     $coleccionJuegos[0]=["jugadorCruz"=>"Igor","jugadorCirculo"=>"Felix", "puntosCruz"=>4,"puntosCirculo"=>0];
